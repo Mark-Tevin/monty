f6f042e48331d48e367bd3aaf6aa305c9238e372
Mark-Tevin <tevinsmark@gmail.com>
2024-03-30 04:35:34 -0700

    errorfiles

--- errors.c
+++ errors.c
@@ -45,3 +45,40 @@ void error(int error_code, ...)
 	free_nodes();
 	exit(EXIT_FAILURE);
 }
+/**
+ * more_error- list of the other file handling error functions.
+ * @error_code: The error code indicates the type of error.
+ *              (6): The stack is empty when executing the "pint" operation.
+ */
+void more_error(int error_code, ...)
+{
+	va_list args;
+	char *op;
+	int line_num;
+
+	va_start(args, error_code);
+	switch (error_code)
+	{
+		case 6:
+			fprintf(stderr, "Line %d: Unable to execute 'pint', stack is empty\n",
+				va_arg(args, int));
+			break;
+		case 7:
+			fprintf(stderr, "Line %d: Unable to execute 'pop', the stack is empty\n",
+				va_arg(args, int));
+			break;
+		case 8:
+			line_num = va_arg(args, unsigned int);
+			op = va_arg(args, char *);
+			fprintf(stderr, "L%d: Unable to execute '%s', stack is short", line_num, op);
+			break;
+		case 9:
+			fprintf(stderr, "Line %d: Division by zero error\n",
+				va_arg(args, unsigned int));
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
