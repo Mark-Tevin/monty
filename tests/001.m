cf578ee8d0145592af4fe090721de9d2f0eee011
Mark-Tevin <tevinsmark@gmail.com>
2024-03-31 05:01:11 -0700

    monty

---
+++ tests/001.m
@@ -0,0 +1,12 @@
+# Test Case 1: Pushing values onto the stack and printing them
+push 0
+push 1
+push 2
+push 3
+pall
+
+# Test Case 2: Pushing additional values onto the stack and printing them
+push 4
+push 5
+push 6
+pall This is the end of our program. Monty is awesome!
